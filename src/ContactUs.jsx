fc55c6aee4366ae88b8ca22ed3724575d18f5446
Jack Sunderman <jacksunderman@Jacks-MacBook-Air.local>
2023-09-15 10:57:23 -0400

    contact us form

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -3,7 +3,27 @@ const ContactUs = () => {
         <>
         <h1> Contact Us:</h1>
         <form>
-
+            <label> First Name:
+                <input type="text" name="first" placeholder="First Name">
+                   
+                </input>
+            </label>
+            <label> Last Name:
+                <input type="text" name="last" placeholder="Last Name">
+                    
+                </input>
+            </label>
+            <label> Email:
+                <input type="text" name="email" placeholder="enter email">
+                  
+                </input>
+            </label>
+            <label> Message:
+                <input type="text" name="message" placeholder="message">
+                    
+                </input>
+            </label>
+            <button> Submit </button>
         </form>
         </>
     )
