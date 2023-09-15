24cec9e034095b8d436ad256bbf162c37c25d68f
Jack Sunderman <jacksunderman@Jacks-MacBook-Air.local>
2023-09-15 11:40:15 -0400

    usernames added to each post and location finished

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -5,12 +5,13 @@ const Posts = ({ posts, auth })=> {
     <ul>
       {
         posts.map( post => {
+          console.log(post)
           return (
             <li key={ post._id } className={ post.author._id === auth._id ? 'mine': ''}>
               <Link to={`/posts/${post._id}`}>{ post.title }</Link> 
               <br/> Price: ${ (post.price*1).toFixed(2) } 
-               <br/> created by:
-              <br/> from: {post.location}
+               <br/> created by: {post.author.username}
+              <br/> from: {post.location === "[On Request]" ? 'Location Unknown' : post.location}
             </li>
           );
         })
